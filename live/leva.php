481ce339a351e6e79f33c1a08646a6179da4269b
Spartvan <85900278+sprt-09mftv@users.noreply.github.com>
2025-06-08 20:08:42 +0300

    Create leva.php

---
+++ live/leva.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=UTF-8>
+<meta name=viewport content="width=device-width, initial-scale=1.0">
+<title>Телеканал Лёва ТВ</title>
+<link rel=stylesheet href=/css/video.css>
+<script type="fa17f6d9b113e2f8340a0536-text/javascript">var meta=document.createElement("meta");meta.name="referrer",meta.content="never",document.getElementsByTagName("head")[0].appendChild(meta);</script>
+</head>
+<body>
+<div id=video-container tabindex=0>
+<div class=loader id=loader></div>
+<video id=video autoplay></video>
+<div class=controls id=video-controls>
+<div class=control-items>
+<button id=playPause><svg class=playPause viewBox="0 0 26 26" version=1.1 xmlns=http://www.w3.org/2000/svg xmlns:xlink=http://www.w3.org/1999/xlink x=0 y=0><path d=M4,1.5h6v23H4V1.5z></path><path d=M16,1.5h6v23h-6V1.5z></path></svg></button>
+<input type=range id=volume class=slider min=0 max=1 step=0.05 value=1>
+<button id=settings><svg class=settings viewBox="0 0 26 26" version=1.1 xmlns=http://www.w3.org/2000/svg xmlns:xlink=http://www.w3.org/1999/xlink x=0 y=0><path d="M22.4,13c0-1.2,1.7-2.8,2.6-3.6c-0.6-1.7-1.5-3.3-2.8-4.6c-1.1,0.4-3.4,1-4.5,0.4c-1.1-0.6-1.7-2.8-1.9-3.9 C14.9,1.1,14,1,13,1s-1.9,0.1-2.8,0.3C10,2.4,9.4,4.6,8.3,5.2c-1.1,0.6-3.3,0-4.5-0.4C2.5,6.2,1.6,7.7,1,9.4 c0.9,0.8,2.6,2.3,2.6,3.6S1.9,15.8,1,16.6c0.6,1.7,1.5,3.3,2.8,4.6c1.1-0.4,3.4-1,4.5-0.4c1.1,0.6,1.7,2.8,1.9,3.9 C11.1,24.9,12,25,13,25s1.9-0.1,2.8-0.3c0.2-1.1,0.8-3.3,1.9-3.9c1.1-0.6,3.3,0,4.5,0.4c1.3-1.3,2.2-2.9,2.8-4.6 C24.1,15.8,22.4,14.2,22.4,13z M13,17c-2.2,0-4-1.8-4-4s1.8-4,4-4s4,1.8,4,4S15.2,17,13,17z"></path></svg><span id=currentQuality>Авто</span></button>
+<button id=fullscreen><svg class=fullscreen viewBox="0 0 26 26"><polygon points="3.5,15.5 0.5,15.5 0.5,19.5 0.5,22.5 3.5,22.5 8.5,22.5 8.5,19.5 3.5,19.5 	"></polygon><path d=M22.5,19.5h-5v3h5h3v-3v-4h-3C22.5,15.5,22.5,17.6,22.5,19.5z></path><path d=M22.5,3.5h-5v3h5c0,2,0,4,0,4h3v-4v-3H22.5z></path><polygon points="0.5,3.5 0.5,6.5 0.5,10.5 3.5,10.5 3.5,6.5 8.5,6.5 8.5,3.5 3.5,3.5"></polygon></svg></button>
+</div>
+<div id=settingsMenu></div>
+</div>
+</div>
+<script type="fa17f6d9b113e2f8340a0536-text/javascript">var streams = ["https://msk2.peers.tv/streaming/lyova/16/g2500/playlist.m3u8?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhY2Nlc3MiLCJleHAiOjE3NDk0ODc4MTUsImlhdCI6MTc0OTQwMTQxNSwiY2lkIjo4MSwidWlkIjo5MjI0ODYyMjAsInJlZyI6ZmFsc2UsImlwIjoxNjA0NTc4NjQxfQ.bJ3ofd-h5WiOQde6AYJjgER2hwhCtYTWLHWhrfh7Dts"];
+function isIOS() {return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;}
+var script = document.createElement('script');
+script.src = isIOS() ? '/js/ios.js' : '/js/hls.js';
+document.head.appendChild(script);
+</script>
+<script src="/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="fa17f6d9b113e2f8340a0536-|49" defer></script><script>(function(){function c(){var b=a.contentDocument||a.contentWindow.document;if(b){var d=b.createElement('script');d.innerHTML="window.__CF$cv$params={r:'94c9e31d8f84a860',t:'MTc0OTQwMTQxNi4wMDAwMDA='};var a=document.createElement('script');a.nonce='';a.src='/cdn-cgi/challenge-platform/scripts/jsd/main.js';document.getElementsByTagName('head')[0].appendChild(a);";b.getElementsByTagName('head')[0].appendChild(d)}}if(document.body){var a=document.createElement('iframe');a.height=1;a.width=1;a.style.position='absolute';a.style.top=0;a.style.left=0;a.style.border='none';a.style.visibility='hidden';document.body.appendChild(a);if('loading'!==document.readyState)c();else if(window.addEventListener)document.addEventListener('DOMContentLoaded',c);else{var e=document.onreadystatechange||function(){};document.onreadystatechange=function(b){e(b);'loading'!==document.readyState&&(document.onreadystatechange=e,c())}}}})();</script><script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015" integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ==" data-cf-beacon='{"rayId":"94c9e31d8f84a860","serverTiming":{"name":{"cfExtPri":true,"cfEdge":true,"cfOrigin":true,"cfL4":true,"cfSpeedBrain":true,"cfCacheStatus":true}},"version":"2025.5.0","token":"d73e9ea472c04f299c7559542079eb78"}' crossorigin="anonymous"></script>
+</body>
+</html>
